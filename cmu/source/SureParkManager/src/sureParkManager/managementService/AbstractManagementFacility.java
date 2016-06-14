7941de0411eb003413768a7b86b6317e291cd387
Jaeheon Kim <acmoleg@gmail.com>
2016-06-14 19:09:27 -0400

    SureParkManager : Added  send event to web service when updateslotstatus event and detectFailure event is occured.

--- cmu/source/SureParkManager/src/sureParkManager/managementService/AbstractManagementFacility.java
+++ cmu/source/SureParkManager/src/sureParkManager/managementService/AbstractManagementFacility.java
@@ -6,5 +6,5 @@ public abstract class AbstractManagementFacility{
 	public abstract void updateSlotStatus(int garageID, int slotIdx, int slotStatus) throws Exception;
 	
 	// CS -> MS 
-	public abstract void setFacilityFailure(int garageID, boolean isFail);
+	public abstract void setFacilityFailure(int garageID, boolean isFail) throws Exception;
 }
--- cmu/source/SureParkManager/src/sureParkManager/managementService/ManagementFacility.java
+++ cmu/source/SureParkManager/src/sureParkManager/managementService/ManagementFacility.java
@@ -1,24 +1,42 @@
 package sureParkManager.managementService;
 
+import org.json.simple.JSONObject;
 import sureParkManager.common.SureParkConfig;
 
 public class ManagementFacility extends AbstractManagementFacility {
+	ServerSender serverSender = null;
+
+	public ManagementFacility(){
+		serverSender = new ServerSender();
+	}
+
 	// CS -> MS
 	public void updateSlotStatus(int garageID, int slotIdx, int slotStatus)  throws Exception
 	{
 		SureParkConfig config = SureParkConfig.getInstance();
 		config.setGarageSlotState(garageID, slotIdx, slotStatus);
+
+		JSONObject jsonObject = new JSONObject();
+
+		jsonObject.put("updateSlotStatus", new JSONObject().put("garageID", garageID));
+
+		serverSender.write(jsonObject.toJSONString());
 		
 		System.out.println("update slot state");
 	}
 	
 	// CS -> MS
-	public void setFacilityFailure(int garageID, boolean isFail)
-	{
+	public void setFacilityFailure(int garageID, boolean isFail) throws Exception {
 		if (isFail)
 			System.out.println("EVENT : " + garageID +" facility is broken...");
 		else
 			System.out.println("EVENT : " + garageID +" facility is fixed...");
+
+		JSONObject jsonObject = new JSONObject();
+
+		jsonObject.put("detectFailure", new JSONObject().put("garageID", garageID));
+
+		serverSender.write(jsonObject.toJSONString());
 
 	}
 }
