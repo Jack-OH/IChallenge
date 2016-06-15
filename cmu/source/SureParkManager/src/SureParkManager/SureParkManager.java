1466388120442b592f4a0551793d35e26db996c1
joan.kim <joan.kim@lginnotek.com>
2016-06-15 14:06:37 -0400

    simulate open gate

--- cmu/source/SureParkManager/src/SureParkManager/SureParkManager.java
+++ cmu/source/SureParkManager/src/SureParkManager/SureParkManager.java
@@ -2,6 +2,10 @@ package sureParkManager;
 
 import sureParkManager.managementService.AbstractManagementFacility;
 import sureParkManager.managementService.ManagementFacility;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 import sureParkManager.common.SureParkConfig;
 import sureParkManager.controlService.ControlService;
 import sureParkManager.managementService.ServerReceiver;
@@ -38,8 +42,18 @@ public class SureParkManager {
         
         config.printGarageList();
         */
-                    
+        
+        // It's test for Open gate
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         while (true) {
+        	
+			String message = in.readLine();
+			
+			if( message.equals("GATE") ) {
+				ctlService.openEntryGate(1001, 1);
+			} else if(message.equals("EXIT")) {
+				break;
+			}	
             // Wait maximum of 1 second
             // for MessageLoop thread
             // to finish.
