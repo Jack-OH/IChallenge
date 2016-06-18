493c1120c7beded33babf36c25552093f8beec17
Jaeheon Kim <acmoleg@gmail.com>
2016-06-18 16:09:20 -0400

    SureParkManager : Modified DB trasactions.

---
+++ cmu/source/SureParkManager/src/sureParkManager/common/ReservationInfo.java
@@ -0,0 +1,19 @@
+package sureParkManager.common;
+
+import java.util.Date;
+
+/**
+ * Created by jaeheonkim on 2016. 6. 18..
+ */
+public class ReservationInfo {
+
+    public Date     reservationTime;
+    public int      gracePeriod;
+    public String   confirmInformation;
+
+    public ReservationInfo (Date reservationTime, int gracePeriod, String confirmInformation) {
+        this.reservationTime = reservationTime;
+        this.gracePeriod = gracePeriod;
+        this.confirmInformation = confirmInformation;
+    }
+}
--- cmu/source/SureParkManager/src/sureParkManager/managementService/ManagementDBTransaction.java
+++ cmu/source/SureParkManager/src/sureParkManager/managementService/ManagementDBTransaction.java
@@ -5,7 +5,11 @@ import com.mongodb.util.JSON;
 
 import sureParkManager.common.GarageInfo;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.Locale;
 
 import com.mongodb.BasicDBList;
 import com.mongodb.BasicDBObject;
@@ -13,6 +17,7 @@ import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.DBObject;
 import com.mongodb.DBCursor;
+import sureParkManager.common.ReservationInfo;
 
 public class ManagementDBTransaction {
 	private static ManagementDBTransaction instance = null;
@@ -58,7 +63,7 @@ public class ManagementDBTransaction {
 		DBCursor cursor = coll.find();
 		while (cursor.hasNext()) {
 			DBObject dbObj = cursor.next();
-			GarageInfo garageInfo = new GarageInfo((int) dbObj.get("garageID"), (String) dbObj.get("garageName"),
+			GarageInfo garageInfo = new GarageInfo((int) dbObj.get("garageNumber"), (String) dbObj.get("garageName"),
 					(String) dbObj.get("garageIP"), (int) dbObj.get("slotNumber"));
 			garageInfoList.add(garageInfo);
 		}
@@ -70,24 +75,61 @@ public class ManagementDBTransaction {
 		DBCollection coll = db.getCollection("garages");
 
 		DBObject dbObject = (DBObject) JSON.parse(str);
+        BasicDBObject doc = new BasicDBObject();
+        doc.append("garageName", dbObject.get("garageName"));
+        doc.append("garageNumber", dbObject.get("garageNumber"));
+        doc.append("slotNumber", dbObject.get("slotNumber"));
+        BasicDBList docList = new BasicDBList();
+        for(int i=0; i < (int)dbObject.get("slotNumber"); i++) {
+            docList.add("Open");
+        }
+        doc.append("slotStatus", docList);
+        doc.append("updateTime", new Date());
+        doc.append("gracePeriod", dbObject.get("gracePeriod"));
+        doc.append("parkingFee", 5);
+        doc.append("garageIP", dbObject.get("garageIP"));
+        doc.append("isAvailable", dbObject.get("isAvailable"));
 
-		coll.insert(dbObject);
+		coll.insert(doc);
 	}
 
 	public void addNewReservation(String str) throws Exception {
 		DBCollection coll = db.getCollection("reservations");
 
 		DBObject dbObject = (DBObject) JSON.parse(str);
+		BasicDBObject doc = new BasicDBObject();
 
-		coll.insert(dbObject);
+        doc.append("userID", dbObject.get("userID"));
+        doc.append("reservationTime", new Date());
+        doc.append("cardInfo", dbObject.get("cardInfo"));
+        doc.append("confirmInformation", dbObject.get("confirmInformation"));
+        doc.append("gracePeriod", dbObject.get("gracePeriod"));
+        doc.append("reservationStatus", "waiting");
+        doc.append("parkingFee", 5);
+        doc.append("usingGarage", dbObject.get("usingGarage"));
+        doc.append("usingSlot", dbObject.get("usingSlot"));
+        doc.append("parkingTime", "null");
+        doc.append("leaveTime", "null");
+        doc.append("chargingFee", 0);
+
+		coll.insert(doc);
 	}
 
 	public void addNewUser(String str) throws Exception {
 		DBCollection coll = db.getCollection("users");
 
 		DBObject dbObject = (DBObject) JSON.parse(str);
+        BasicDBObject doc = new BasicDBObject();
 
-		coll.insert(dbObject);
+        doc.append("userID", dbObject.get("userID"));
+        doc.append("userPassword", dbObject.get("userPassword"));
+        doc.append("userType", dbObject.get("userType"));
+        doc.append("userName", dbObject.get("userName"));
+        doc.append("userEmail", dbObject.get("userEmail"));
+        doc.append("registerationTime", new Date());
+        doc.append("displayName", dbObject.get("displayName"));
+
+		coll.insert(doc);
 	}
 
 	public void updateGarageSlot(int garageID, ArrayList<Integer> slotStatus) {
@@ -104,6 +146,14 @@ public class ManagementDBTransaction {
 
 		coll.update(new BasicDBObject().append("garageID", garageID), updateObj);
 	}
+
+    public ArrayList<ReservationInfo> getReservationInfo() {
+        ArrayList<ReservationInfo> reservationInfoList = null;
+
+        // TODO: implement this function.
+
+        return reservationInfoList;
+    }
 
 	public void disconnectDB() throws Exception {
 		mongoClient.close();
